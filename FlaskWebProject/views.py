cdcfb3d016727f253a5684f023fa442a150cc79c
faubsam <faubertsamuel@gmail.com>
2022-10-11 19:51:12 -0400

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -70,14 +70,14 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        
+        app.logger.warning(f'Successful login by user: {user}')
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
-    app.logger.warning(f'Successful login by user: {user}')
+    
     return render_template('login.html', title='Sign In', form=form, auth_url=auth_url)
 
 @app.route(Config.REDIRECT_PATH)  # Its absolute URL must match your app's redirect_uri set in AAD
